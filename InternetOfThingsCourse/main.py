6315f627afbecfb3f866bf57545e99526a4b0433
Miguel Tlapa Juarez <miguel.tlapa.juarez@intel.com>
2016-05-07 05:06:49 +0000

    P4 Mosquitto Publisher One Thread

--- InternetOfThingsCourse/main.py
+++ InternetOfThingsCourse/main.py
@@ -1,4 +1,6 @@
-# P1 One Thread Data Network
+#!/usr/bin/python
+#P4_Mosquitto_Publisher_One_Thread
+import paho.mqtt.client as paho
 import psutil
 import signal
 import sys
@@ -9,16 +11,23 @@ from threading import Thread
 def interruptHandler(signal, frame):
     sys.exit(0)
 
+def on_publish(mosq, obj, msg):
+    pass
+
 def dataNetwork():
     netdata = psutil.net_io_counters()
     return netdata.packets_sent + netdata.packets_recv
 
 def dataNetworkHandler():
-    idDevice = "IoT101Device"
+    idDevice = "ThisDevice"
+    mqttclient = paho.Client()
+    mqttclient.on_publish = on_publish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
         packets = dataNetwork()
         message = idDevice + " " + str(packets)
-        print "dataNetworkHandler " + message
+        print "MQTT dataNetworkHandler " + message
+        mqttclient.publish("IoT101/Network", message)
         time.sleep(1)
 
 if __name__ == '__main__':
@@ -32,4 +41,4 @@ if __name__ == '__main__':
         print "Hello Internet of Things 101"
         time.sleep(5)
 
-# End of File
+
