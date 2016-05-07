471a577f0ba32462a5f3aa27d8c4d96533403bf6
Miguel Tlapa Juarez <miguel.tlapa.juarez@intel.com>
2016-05-07 03:21:16 +0000

    1st Data Sensor Program

--- InternetOfThingsCourse/main.py
+++ InternetOfThingsCourse/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
+import psutil
 import signal
 import sys
 import time
+
+def functionDataSensor():
+    netdata = psutil.net_io_counters()
+    data = netdata.packets_sent + netdata.packets_recv
+    return data
 
 def functionSignalHandler(signal, frame):
     sys.exit(0)
@@ -13,6 +19,7 @@ if __name__ == '__main__':
 
     while True:
         print "Hello Internet of Things 101"
+        print "Data Sensor: %s " % functionDataSensor()
         time.sleep(5)
 
 # End of File
