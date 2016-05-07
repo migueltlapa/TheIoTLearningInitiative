89e91bc476d9ffd0302749da237e97d95bf1c6bf
Miguel Tlapa Juarez <miguel.tlapa.juarez@intel.com>
2016-05-07 02:49:31 +0000

    Adding Signal Handler

--- InternetOfThingsCourse/main.py
+++ InternetOfThingsCourse/main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
+import signal
+import sys
 import time
 
+def functionSignalHandler(signal, frame):
+    sys.exit(0)
+
 if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, functionSignalHandler)
 
     while True:
         print "Hello Internet of Things 101"
