23acc20681e577ff264e1d0d9c037f76f05a7a8c
Miguel Tlapa Juarez <miguel.tlapa.juarez@intel.com>
2016-05-07 05:04:05 +0000

    P2 Flask Network Data Send Dummy Data

--- InternetOfThingsCourse/mainflask.py
+++ InternetOfThingsCourse/mainflask.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
-
+#P2 Flask Network Data, Send Dummy Data
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-class DataSensorRestApi(Resource):
+class Network(Resource):
     def get(self):
-        data = 'This is data from a sensor'
+        data = 'Network Data'
         return data
 
-api.add_resource(DataSensorRestApi, '/datasensor')
+api.add_resource(Network, '/network')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
